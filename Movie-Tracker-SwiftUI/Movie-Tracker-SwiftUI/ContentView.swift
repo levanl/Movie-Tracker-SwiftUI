adcc2d7597991fba1baf2dae2c7f706b6544f8ee
Levan Loladze <levan.loladzew@gmail.com>
2024-01-05 18:28:10 +0400

    .DS_Store banished!

---
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie-Tracker-SwiftUI
+//
+//  Created by Levan Loladze on 05.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Movie_Tracker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Tracker_SwiftUIApp.swift
+//  Movie-Tracker-SwiftUI
+//
+//  Created by Levan Loladze on 05.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Tracker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

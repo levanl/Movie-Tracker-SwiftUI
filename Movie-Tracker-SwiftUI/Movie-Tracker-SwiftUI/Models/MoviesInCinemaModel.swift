0fa8482e5eff9225d2611e956d15db9819c3fd08
Anna Sumire <anachikhladze99@gmail.com>
2024-01-06 19:23:14 +0400

    initial commit

--- Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Models/MoviesInCinemaModel.swift
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Models/MoviesInCinemaModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+// MARK: - MoviesInCinemaResponse
+struct MoviesInCinemaResponse: Decodable {
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Decodable, Identifiable, Hashable {
+    var id = UUID()
+    let originalTitle, overview: String
+    let popularity: Double
+    let posterPath, releaseDate: String
+    let voteAverage: Double
+    let voteCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
--- Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/Movies in Cinema/MoviesInCinemaViewModel.swift
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/Movies in Cinema/MoviesInCinemaViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import NetworkManager
+import SwiftUI
+
+// MARK: - MoviesInCinemaViewModel
+final class MoviesInCinemaViewModel: ObservableObject {
+    
+    // MARK: - Properties
+    @Published var moviesInCinema: [Result] = []
+    
+    // MARK: - Init
+    init() {
+        Task {
+            await fetchMoviesInCinema()
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func fetchMoviesInCinema() async {
+        let urlString = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=a7105b611883e6587847c2feebcbf151"
+        do {
+            let moviesInCinemaResponse: MoviesInCinemaResponse = try await NetworkManager.shared.fetchData(fromURL: urlString)
+            await MainActor.run {
+                moviesInCinema = moviesInCinemaResponse.results
+            }
+        } catch {
+            print(error)
+        }
+    }
+}

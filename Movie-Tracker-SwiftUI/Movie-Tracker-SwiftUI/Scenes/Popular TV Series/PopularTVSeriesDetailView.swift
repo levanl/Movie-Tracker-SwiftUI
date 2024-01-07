590412f1b17377d3844450394ef553ba156e5b63
Levan Loladze <levan.loladzew@gmail.com>
2024-01-07 11:19:07 +0400

    Add DetailsPage and Navigation, Mark up Code

---
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/Popular TV Series/PopularTVSeriesDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PopularTVSeriesDetailView.swift
+//  Movie-Tracker-SwiftUI
+//
+//  Created by Levan Loladze on 07.01.24.
+//
+
+import SwiftUI
+
+struct PopularTVSeriesDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopularTVSeriesDetailView()
+}

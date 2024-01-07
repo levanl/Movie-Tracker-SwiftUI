cd0b5412b451ceb34081fc0a7fdf81b4c8cdc33b
Levan Loladze <89299000+levanl@users.noreply.github.com>
2024-01-07 21:44:47 +0400

    Revert "Tv series on air pull request"

--- Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/TV Series On Air/On Air Components/OnAirComponent.swift
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/TV Series On Air/On Air Components/OnAirComponent.swift
@@ -6,71 +6,13 @@
 //
 
 import SwiftUI
-import NetworkManager
 
-
-struct TVSeriesOnAirCell: View {
-    let TVSeriesOnAirmodel: TVSeriesOnAirModel
-
+struct OnAirComponent: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            AsyncImage(url: URL(string: TVSeriesOnAirmodel.TVSeriesOnAirImage)) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            } placeholder: {
-                ProgressView()
-            }
-            .aspectRatio(contentMode: .fill)
-            .frame(maxHeight: 1000)
-            .cornerRadius(15)
-            .shadow(radius: 50)
-
-            Text(TVSeriesOnAirmodel.TVSeriesOnAirName)
-                .font(.headline)
-                .padding(.top, 4)
-                .padding(.bottom, 2)
-
-            HStack {
-                Text("\(TVSeriesOnAirmodel.TVSeriesOnAirYear)")
-                    .font(.subheadline)
-                Spacer()
-                Text("\(TVSeriesOnAirmodel.TVSeriesOnAirLanguage)")
-                    .font(.subheadline)
-            }
-            .foregroundColor(.gray)
-            
-        }
-        .shadow(radius: 20)
-        .padding()
-        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct OnAirTVSeriesSearchBar: View {
-    @Binding var onAirTvSeriesSearchText: String
-
-    var body: some View {
-        HStack {
-            TextField("Search for TV Series on air right now", text: $onAirTvSeriesSearchText)
-                .padding(8)
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-                .padding(.horizontal, 10)
-                .padding(.vertical, 5)
-
-            Spacer()
-
-            if !onAirTvSeriesSearchText.isEmpty {
-                Button(action: {
-                    onAirTvSeriesSearchText = ""
-                }) {
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(.gray)
-                }
-                .padding(.trailing, 10)
-                .transition(.move(edge: .trailing))
-            }
-        }
-    }
+#Preview {
+    OnAirComponent()
 }
--- Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/TV Series On Air/TVSeriesOnAirView.swift
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/TV Series On Air/TVSeriesOnAirView.swift
@@ -8,90 +8,11 @@
 import SwiftUI
 
 struct TVSeriesOnAirView: View {
-    @ObservedObject private var viewModel = TVSeriesOnAirViewModel()
-    @State private var selectedTVSeries: TVSeriesOnAirModel? = nil
-    @State private var searchText: String = ""
-
-    var filteredTVSeries: [TVSeriesOnAirModel] {
-        if searchText.isEmpty {
-            return viewModel.TVSeriesOnAir
-        } else {
-            return viewModel.TVSeriesOnAir.filter {
-                $0.TVSeriesOnAirName.localizedCaseInsensitiveContains(searchText)
-            }
-        }
-    }
-
     var body: some View {
-        VStack {
-            Spacer()
-            OnAirTVSeriesSearchBar(onAirTvSeriesSearchText: $searchText)
-
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
-                    ForEach(filteredTVSeries, id: \.TVSeriesOnAirName) { series in
-                        TVSeriesOnAirCell(TVSeriesOnAirmodel: series)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: .infinity)
-                            .onTapGesture {
-                                selectedTVSeries = series
-                            }
-                    }
-                }
-                .padding()
-            }
-            .sheet(item: $selectedTVSeries) { series in
-                TVSeriesOnAirDetailView(tvSeries: series)
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-
-struct TVSeriesOnAirDetailView: View {
-    let tvSeries: TVSeriesOnAirModel
-
-    var body: some View {
-        
-        VStack {
-            Spacer()
-            Text(tvSeries.TVSeriesOnAirName)
-                .font(.title)
-                .bold()
-                .padding()
-
-            HStack {
-                Text("Release: \(tvSeries.TVSeriesOnAirYear)")
-                Spacer()
-                Text("Original Languge: \(tvSeries.TVSeriesOnAirLanguage)")
-            } .padding(.leading)
-            .padding(.trailing)
-
-            AsyncImage(url: URL(string: tvSeries.TVSeriesOnAirImage)) { image in
-                image.resizable()
-                    .aspectRatio(contentMode: .fit)
-            } placeholder: {
-                ProgressView()
-            }
-            .cornerRadius(15)
-            .padding()
-            
-            ZStack {
-                LinearGradient(
-                    gradient: Gradient(colors: [Color.gray.opacity(70), Color.white.opacity(2)]),
-                    startPoint: .bottom,
-                    endPoint: .top
-                )
-                .edgesIgnoringSafeArea(.all)
-                
-                Text(tvSeries.TVSeriesOnAirOverview)
-                    .foregroundColor(.black)
-                    .padding(40)
-                
-                Spacer()
-            }
-
-        }
-    }
+#Preview {
+    TVSeriesOnAirView()
 }
-

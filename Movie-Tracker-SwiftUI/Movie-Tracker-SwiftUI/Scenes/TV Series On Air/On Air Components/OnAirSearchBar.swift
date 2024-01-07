fdab03140e072546599e323d0be2e38162377391
slapiashvili <sslapiashvili@gmail.com>
2024-01-07 18:38:53 +0400

    refine code according to the reviews

---
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/TV Series On Air/On Air Components/OnAirSearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  OnAirSearchBar.swift
+//  Movie-Tracker-SwiftUI
+//
+//  Created by Salome Lapiashvili on 07.01.24.
+//
+
+import SwiftUI
+
+struct OnAirSearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnAirSearchBar()
+}
---
+++ Movie-Tracker-SwiftUI/Movie-Tracker-SwiftUI/Scenes/TV Series On Air/TVSeriesOnAirDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TVSeriesOnAirDetailView.swift
+//  Movie-Tracker-SwiftUI
+//
+//  Created by Salome Lapiashvili on 07.01.24.
+//
+
+import SwiftUI
+
+struct TVSeriesOnAirDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TVSeriesOnAirDetailView()
+}
